11766549aa8cd89fd1a75e0d78c57eb29ca33c68
FirasChalbi <firaschalbi0@gmail.com>
2023-09-18 11:12:10 +0200

    Fix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from "./container/Header"
-import Search from './container/Search';
+import Search from './container/Search'
 
 function App() {
   return (
--- src/container/Search.js
+++ src/container/Search.js
@@ -1,35 +1,44 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import "./search.css";
 
 function Search() {
     const [username, setUsername] = useState('');
-    const [user, setUser] = useState({})
-    fetch(`https://api.github.com/users/${username}`)
-    //.then((response) => setUser(response))
-    .then((user) => user.json())
-    .then((user) => setUser(user));
+    const [user, setUser] = useState(null);
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
-      };
-    
-    console.log(username);
-  return (
-    <div className='body'>
-    <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="Github Username" onChange={e => setUsername(e.target.value)} required={true} />
-        <button type="submit" >Search</button>
-    </form>
-    <div className='info'>
-    <img src={user.avatar_url} width="70" height="70" ></img>
-    <div>
-      Name: {user.login} <br/> Followers: {user.followers} <br/> Repositories count: {user.public_repos}
-    </div>
-    </div>
-    </div>
 
-    
-  )
+        if (username) {
+            try {
+                const response = await fetch(`https://api.github.com/users/${username}`);
+                if (response.ok) {
+                    const data = await response.json();
+                    setUser(data);
+                } else {
+                    console.error('Error fetching user data');
+                }
+            } catch (error) {
+                console.error('Error:', error);
+            }
+        }
+    };
+
+    return (
+        <div className='body'>
+            <form onSubmit={handleSubmit}>
+                <input type="text" placeholder="Github Username" onChange={e => setUsername(e.target.value)} required={true} />
+                <button type="submit">Search</button>
+            </form>
+            {user && (
+                <div className='info'>
+                    <img src={user.avatar_url} width="70" height="70" alt="User Avatar" />
+                    <div>
+                        Name: {user.login} <br/> Followers: {user.followers} <br/> Repositories count: {user.public_repos}
+                    </div>
+                </div>
+            )}
+        </div>
+    );
 }
 
-export default Search
+export default Search;
